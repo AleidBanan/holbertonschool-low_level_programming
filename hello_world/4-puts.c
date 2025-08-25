e32267965456dc5787ac77acfc1889084a800962
AleidBanan <Aleidbanan@gmail.com>
2025-08-25 10:38:10 +0000

    puts task 4 llp

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints a sentence with proper grammar using puts
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
+	return (0);
 }
 
