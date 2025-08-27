bbb95f3ade094d5482ac45c2fb9ee27d1da019a0
AleidBanan <Aleidbanan@gmail.com>
2025-08-27 16:02:09 +0000

    lower task 3 loops

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
 * main - check the code
@@ -6,12 +7,15 @@
 * Return: Always 0.
 */
 int main(void)
-
 {
+	int r;
 
-	print_alphabet_x10();
-
+	r = _islower('H');
+	printf("%d\n", r);
+	r = _islower('o');
+	printf("%d\n", r);
+	r = _islower(108);
+	printf("%d\n", r);
 	return (0);
-
 }
 
