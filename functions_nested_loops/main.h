1dec33a196b60b13139663c4201d44efa73a6bcc
AleidBanan <Aleidbanan@gmail.com>
2025-08-28 12:14:47 +0000

    abs task 6 loops

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,5 +3,10 @@
 
 int _putchar(char c);
 int print_sign(int n);
+int _islower(int c);
+int _isalpha(int c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _abs(int);
 
-#endif /* MAIN_H */
+#endif
