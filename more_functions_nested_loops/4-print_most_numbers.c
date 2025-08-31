151fa8da950f3b4564ec860d55c15c9d42a8be88
AleidBanan <Aleidbanan@gmail.com>
2025-08-31 16:49:36 +0000

    print task 4 nested

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+*  prints the numbers, from 0 to 9
+*
+* Return: number
+*/
+void print_most_numbers(void);
+{
+	char c;
+
+	for (c = 0; c < 10; c++)
+
+	{
+
+	If (c != 2 && c != 4)
+
+	}
+
+	_putchar('0' + c);
+
+	}
+
+	_putchar('\n');
+
+}
