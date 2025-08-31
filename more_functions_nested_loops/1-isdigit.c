7ac778adfade5f46ef05e05476368b4aa1af1baf
AleidBanan <Aleidbanan@gmail.com>
2025-08-31 14:58:43 +0000

    digit task 1 nested

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isdigit - checks if a character is digit
+* @c: The digit to check
+*
+* Return: 1 if c is digit, 0 otherwise
+*/
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
