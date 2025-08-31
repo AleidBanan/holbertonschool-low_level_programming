f2203bc9cd080b24e6992b9c7a9c41b9ec2a77ad
AleidBanan <Aleidbanan@gmail.com>
2025-08-31 16:13:53 +0000

    print task 3 nested

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_numbers – print numbers
+*
+*Description: Uses _putchar to print each digit
+*
+* Return: character
+*/
+void print_numbers(void)
+{
+	 char c;
+
+	for (c = 0; c < 10; c++)
+	{
+		_putchar('0' + c);
+		_putchar('\n');
+
+	}
+
+}
