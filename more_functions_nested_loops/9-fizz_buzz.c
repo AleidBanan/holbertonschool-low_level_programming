7bebfc80f072ce80e77428e8b2e6e0b8ce06c852
AleidBanan <Aleidbanan@gmail.com>
2025-09-01 12:42:48 +0000

    frizz task 9 nested

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+* print_number - prints an integer using _putchar
+* @n: the number to print
+*/
+void print_number(int n)
+{
+	if (n / 10)
+		print_number(n / 10);
+	_putchar('0' + n % 10);
+}
+
+/**
+* main - prints numbers from 1 to 100 with FizzBuzz rules
+*
+* Return: 0
+*/
+int main(void)
+{
+int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (i % 3 == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+		}
+		else if (i % 5 == 0)
+		{
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+		}
+		else
+		{
+			print_number(i);
+		}
+
+		if (i != 100)
+			_putchar(' ');
+	}
+	_putchar('\n');
+return (0);
+}
