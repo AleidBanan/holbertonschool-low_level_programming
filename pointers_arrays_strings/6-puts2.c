b27bf123de43b3f6a065cf99f3aa43d9abd07c4b
AleidBanan <Aleidbanan@gmail.com>
2025-09-02 17:45:58 +0000

    first task 6 array

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -5,13 +5,11 @@
 * @str: pointer to the string
 */
 void puts2(char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+	while (*str)
+	{
+		_putchar(*str);
+		str += 2;
 	}
 	_putchar('\n');
 }
