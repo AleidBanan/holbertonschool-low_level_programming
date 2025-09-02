706f63d0ec95cdb74d35696310f39a9884620383
AleidBanan <Aleidbanan@gmail.com>
2025-09-02 16:27:22 +0000

    puts task 3 array

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,12 +1,19 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
-* Description: _puts - prints a string followed by a new line
+* _puts - prints a string followed by a new line
 * @str: pointer to the string to print
-* Return: nothing
 */
+
 void _puts(char *str)
 {
-	puts(str);
+int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
 }
