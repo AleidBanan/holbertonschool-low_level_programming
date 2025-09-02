4ece8c1306abcb2d5b517cee3ad09220bbccf377
AleidBanan <Aleidbanan@gmail.com>
2025-09-02 16:19:58 +0000

    puts task 3 array

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* Description: _puts - prints a string followed by a new line
+* @str: pointer to the string to print
+* Return: nothing
+*/
+void _puts(char *str)
+{
+	puts(str);
+}
