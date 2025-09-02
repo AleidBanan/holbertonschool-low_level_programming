647758a7573f1724feaf5fe0341eb1a56f3daa7d
AleidBanan <Aleidbanan@gmail.com>
2025-09-02 15:22:10 +0000

    change task 0 array

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void reset_to_98(int *n);
+
+/**
+* reset_to_98 - takes a pointer to an int
+*               and updates the value it points to 98
+* @n: pointer to int
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+
+}
+
