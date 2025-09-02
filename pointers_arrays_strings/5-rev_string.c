3b6b7354a196e05a1f986cf8810fe0320473fcdf
AleidBanan <Aleidbanan@gmail.com>
2025-09-02 17:36:34 +0000

    reverse task 5 array

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,18 +7,24 @@
 void rev_string(char *s)
 {
 
-int start = 0;
-int end = 0;
-char temp;
+	int start = 0;
+	int end = 0;
+	char temp;
 
-while (s[end] != '\0')
-	end++;
-
-	end--;
-
-while (start < end)
+	while (s[end] != '\0')
 	{
+		end++;
+	}
+		end--;
+
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+
 		start++;
 		end--;
+
 	}
 }
