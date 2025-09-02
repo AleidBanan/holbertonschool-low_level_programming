aa8adeab2901ccfeade435ded65737bc58403429
AleidBanan <Aleidbanan@gmail.com>
2025-09-02 17:49:41 +0000

    first task 6 array

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -4,12 +4,15 @@
 * puts2 - prints every other character of a string
 * @str: pointer to the string
 */
+
 void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
 	{
-	while (*str)
-	{
-		_putchar(*str);
-		str += 2;
+		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar('\n');
 }
