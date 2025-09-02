7021251dea3d0e30d6a9263b2fa69987c6dd2127
AleidBanan <Aleidbanan@gmail.com>
2025-09-02 17:29:35 +0000

    reverse task 5 array

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* rev_string - reverses a string in place
+* @s: pointer to the string
+*/
+void rev_string(char *s)
+{
+
+int start = 0;
+int end = 0;
+char temp;
+
+while (s[end] != '\0')
+	end++;
+
+	end--;
+
+while (start < end)
+	{
+		start++;
+		end--;
+	}
+}
