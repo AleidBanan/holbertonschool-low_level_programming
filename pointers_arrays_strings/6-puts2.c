cce40d936b9d5729f877aa55479d73799d2c2c60
AleidBanan <Aleidbanan@gmail.com>
2025-09-03 09:11:32 +0000

    skip task 6 array

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* puts2 - prints every other character of a string
+* puts2 - prints every other character of a string, starting with the first
 * @str: pointer to the string
 */
 void puts2(char *str)
@@ -13,6 +13,5 @@ void puts2(char *str)
 		_putchar(str[i]);
 		i += 2;
 	}
-
-_putchar('\n');
+	_putchar('\n');
 }
