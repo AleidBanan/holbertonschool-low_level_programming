ab14b86eb048758cb85cc2e008d803293a19b7d3
AleidBanan <Aleidbanan@gmail.com>
2025-09-10 13:26:59 +0000

    arguments task 2 arc

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 /**
-* main – prints all argument
+* main - prints all argument
 * @argc: number of arguments
 * @argv: array of argument strings
 *
 * Return: Always 0 (success)
 */
+
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		puts(argv[i]);
 	}
-	return(0);
+	return (0);
 }
