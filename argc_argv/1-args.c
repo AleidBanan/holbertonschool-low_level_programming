a92ffd37a8b1962a978a9039e89a88f98c3113a7
AleidBanan <Aleidbanan@gmail.com>
2025-09-10 13:11:32 +0000

    length task 1 arc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - prints the program length
+* @argc: number of arguments
+* @argv: array of argument strings
+*
+* Return: Always 0 (success)
+*/
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
