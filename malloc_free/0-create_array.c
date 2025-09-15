192aacc8ca67c6906492aa5659465ff602bded49
AleidBanan <Aleidbanan@gmail.com>
2025-09-15 12:32:34 +0000

    malloc task 0 alloc

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	char *arr = malloc(size * sizeof(char));
+	arr = malloc(size * sizeof(char));
 		if (arr == NULL)
 			return (NULL);
 	for (i = 0; i < size; i++)
