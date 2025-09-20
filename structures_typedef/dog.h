bfd0a9f65fd6d087592acc087bdb9aa20a6e6382
AleidBanan <Aleidbanan@gmail.com>
2025-09-20 08:43:58 +0000

    dog task 0 struct

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+/**
+* struct dog - structure that defines a dog
+* @name: dog's name (string)
+* @age: dog's age (float)
+* @owner: dog's owner (string)
+*
+* Description: This struct information about a dog
+*/
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+}
