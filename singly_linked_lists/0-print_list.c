f9d2f6a4017b1233541d8c7e2da73fd079cc4472
AleidBanan <Aleidbanan@gmail.com>
2025-10-07 09:18:29 +0000

    task 0 linked

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -23,9 +23,10 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
-				else printf("[%u] %s\n", h->len, h->str);
+		else
+			printf("[%u] %s\n", h->len, h->str);
 	count++;
 	h = h->next;
 	}
-	return count;
+	return (count);
 }
