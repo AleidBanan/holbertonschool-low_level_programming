05c3ec517a8fd07bc9b08f8508da48d088bbbb9f
AleidBanan <Aleidbanan@gmail.com>
2025-10-09 14:42:10 +0000

    add task 3 linked

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -44,7 +44,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	temp = *head;
 	while (temp->next != NULL)
+	{
 		temp = temp->next;
-		temp->next = new_node;
+	}
+	temp->next = new_node;
 	return (new_node);
 }
