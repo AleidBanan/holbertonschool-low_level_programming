34b295a88c4cd24edc05b1802f16a091f620a9ff
AleidBanan <Aleidbanan@gmail.com>
2025-10-12 12:24:32 +0000

    number task 1 doubly

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,8 +1,7 @@
-#include <stdio.h>
 #include "lists.h"
 
 /**
-* print_dlistint - Prints number of nods in  dlistint_t list.
+* dlistint_len - Prints number of nods in  dlistint_t list.
 * @h: Pointer to the head
 *
 * Return: number of nodes
@@ -11,7 +10,7 @@ size_t dlistint_len(const dlistint_t *h)
 
 {
 
-	size_t count = 0
+	size_t count = 0;
 
 	while (h!= NULL)
 	{
