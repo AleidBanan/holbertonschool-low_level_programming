6eeb82db2828e5eeea2044a4104ecb052b0f3daa
AleidBanan <Aleidbanan@gmail.com>
2025-10-14 12:36:17 +0000

    delete task 8 doubly

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -21,7 +21,9 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (index == 0)
 	{
-		(*head)->prev = NULL;
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
 		free(current);
 		return (1);
 	}
