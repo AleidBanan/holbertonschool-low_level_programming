08f65095708d78e37390ef58139bd344499e1468
AleidBanan <Aleidbanan@gmail.com>
2025-10-16 12:03:28 +0000

    create task 1 file

---
+++ file_io/3-cp.c
@@ -0,0 +1,74 @@
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+/**
+* main - copies the text from file to file.
+* @argc: number of arguments
+* @argv: arguments array
+*
+* Return: 0 on success, exit if failure
+*/
+
+int main(int argc, char *argv[])
+{
+
+int fd_from, fd_to;
+
+char reader[1024];
+
+ssize_t r_bytes, w_bytes;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from);
+		exit(99);
+	}
+	while ((r_bytes = read(fd_from, reader, 1024)) > 0)
+	{
+		w_bytes = write(fd_to, reader, r_bytes);
+		if (w_bytes == -1 || w_bytes != r_bytes)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
+			exit(99);
+		}
+	}
+	if (r_bytes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+	return (0);
+}
